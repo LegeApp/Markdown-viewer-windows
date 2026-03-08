ee314a56002134b0fed86443485ab1381bb8dda3
DK <66110819+LegeApp@users.noreply.github.com>
2026-03-08 14:07:39 -0700

    Delete src directory

--- src/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if cfg!(target_os = "windows") {
-        winres::WindowsResource::new()
-            .set_icon("icon.ico") // Optional: add an icon
-            .compile()
-            .unwrap();
-    }
-}
